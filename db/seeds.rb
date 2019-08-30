3b8db7cd8e2f7fb349db2a0d4f37286781187121
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2019-08-30 12:59:51 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.create!(coupon_code: "123", store: "Store 1")
+Coupon.create!(coupon_code: "abc", store: "Store 2")
+Coupon.create!(coupon_code: "bbd", store: "Store 1")
+Coupon.create!(coupon_code: "east coast family", store: "Store 3")
+Coupon.create!(coupon_code: "bel", store: "Store 4")
+Coupon.create!(coupon_code: "biv", store: "Store 5")
+Coupon.create!(coupon_code: "devoe", store: "Store 1")
